d7a0da99ef613297462aedb7cd2dff19f76d1b88
Kresna Satya <kresnasatya72@gmail.com>
2026-02-22 20:52:06 +0800

    set target platform to macOS 13 in order to be able newest feature available on Swift/Swift UI

--- Package.swift
+++ Package.swift
@@ -5,11 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "ToyStack",
+    platforms: [
+        .macOS(.v13)
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "ToyStack"
-        ),
+        )
     ]
 )
