ab5c233e63a84efb061c5f8f33de239324065b28
Kresna Satya <kresnasatya72@gmail.com>
2026-03-06 09:44:17 +0800

    Re-organize code

--- Sources/ToyStack/ToyStack.swift
+++ Sources/ToyStack/ToyStack.swift
@@ -4,46 +4,6 @@
 @main
 struct ToyStack {
     static func main() async throws {
-        let url = URL("https://example.com")
-        let (headers, content) = try await url.request()
-        print("Headers: ", headers)
-        print("Content: ", content)
-
-        // Test HTMLParser
-        let html = content
-        let parser = HTMLParser(body: html)
-        let root = parser.parse()
-        print(root)
-
-        // Test HTMLParser - part 2
-        let html2 = "<html><body><p>Hello</p></body></html>"
-        let parser2 = HTMLParser(body: html2)
-        let root2 = parser2.parse()
-        print(root2)
-
-        // Test Element and TextNode from DOMNode
-        // Top-down: create parent first, then the children
-        let div = Element(tag: "div", attributes: ["class": "wrapper"], parent: nil)
-        let p = Element(tag: "p", attributes: [:], parent: div)
-        let text = TextNode(text: "You're amazing!", parent: p)
-
-        div.children.append(p)
-        p.children.append(text)
-
-        print(div)
-        print(div.children[0])
-        print(div.children[0].children[0])
-
-        // Test CSS Parser
-        let css = "p { color: red; } div p { margin: 0 }"
-        let rules = CSSParser(css).parse()
-        print(rules.count)
-
-        // Test Rect
-        let rect = Rect(left: 10, top: 20, right: 100, bottom: 80)
-        print("Contains (50, 50): ", rect.containsPoint(50, 50))
-        print("Contains (5, 50): ", rect.containsPoint(5, 50))
-        print("Contains (100, 50): ", rect.containsPoint(100, 50))
-        print("CGRect: ", rect.cgRect)
+        print("Hello from ToyStack!")
     }
 }
