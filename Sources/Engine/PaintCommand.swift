137ddf568a1aba1e280f2bcb406a4a8c1290c3cc
Kresna Satya <halo@kresna.me>
2026-09-19 14:46:40 +0800

    refactor: move extension of PaintCommand into PaintCommand file

--- Sources/Engine/PaintCommand.swift
+++ Sources/Engine/PaintCommand.swift
@@ -1,10 +1,40 @@
 import CoreGraphics
+import CoreText
 
 // MARK: - PaintCommand
 public protocol PaintCommand {
     var rect: Rect { get }
     var parentEffect: VisualEffect? { get set }
     func execute(scroll: CGFloat, renderer: any Renderer)
+}
+
+extension PaintCommand {
+    var contentHash: Int {
+        var hasher: Hasher = Hasher()
+        hasher.combine(String(describing: type(of: self)))
+        hasher.combine(rect.left)
+        hasher.combine(rect.top)
+        hasher.combine(rect.right)
+        hasher.combine(rect.bottom)
+        if let cmd = self as? DrawRect {
+            hasher.combine(cmd.color)
+        } else if let cmd = self as? DrawLine {
+            hasher.combine(cmd.color)
+            hasher.combine(cmd.thickness)
+        } else if let cmd = self as? DrawText {
+            hasher.combine(cmd.text)
+            hasher.combine(cmd.color)
+            hasher.combine(CTFontCopyPostScriptName(cmd.font.ctFont) as String)
+            hasher.combine(CTFontGetSize(cmd.font.ctFont))
+        } else if let cmd = self as? DrawOutline {
+            hasher.combine(cmd.color)
+            hasher.combine(cmd.thickness)
+        } else if let cmd = self as? DrawRRect {
+            hasher.combine(cmd.color)
+            hasher.combine(cmd.radius)
+        }
+        return hasher.finalize()
+    }
 }
 
 // MARK: - DrawRect
--- Sources/Engine/TileStore.swift
+++ Sources/Engine/TileStore.swift
@@ -1,5 +1,4 @@
 import CoreGraphics
-import CoreText
 
 struct TileLayerOrigin: Hashable {
     let left: Int
@@ -87,34 +86,5 @@ final class TileStore: @unchecked Sendable {
             totalPixels -= oldest.value.pixels
             entries.removeValue(forKey: oldest.key)
         }
-    }
-}
-
-extension PaintCommand {
-    var contentHash: Int {
-        var hasher: Hasher = Hasher()
-        hasher.combine(String(describing: type(of: self)))
-        hasher.combine(rect.left)
-        hasher.combine(rect.top)
-        hasher.combine(rect.right)
-        hasher.combine(rect.bottom)
-        if let cmd = self as? DrawRect {
-            hasher.combine(cmd.color)
-        } else if let cmd = self as? DrawLine {
-            hasher.combine(cmd.color)
-            hasher.combine(cmd.thickness)
-        } else if let cmd = self as? DrawText {
-            hasher.combine(cmd.text)
-            hasher.combine(cmd.color)
-            hasher.combine(CTFontCopyPostScriptName(cmd.font.ctFont) as String)
-            hasher.combine(CTFontGetSize(cmd.font.ctFont))
-        } else if let cmd = self as? DrawOutline {
-            hasher.combine(cmd.color)
-            hasher.combine(cmd.thickness)
-        } else if let cmd = self as? DrawRRect {
-            hasher.combine(cmd.color)
-            hasher.combine(cmd.radius)
-        }
-        return hasher.finalize()
     }
 }
